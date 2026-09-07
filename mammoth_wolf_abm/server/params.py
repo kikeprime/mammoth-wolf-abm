8589218dfdaf804aff5ab855b73e7b745cdc535b
Kiké fővezér <hermandavidtamas@gmail.com>
2026-09-07 14:48:50 +0200

    Create MammothAgent (#7)
    * Create MammothAgent with basic functionality
    
    * Allow 0 day mammoth calves
    
    * Add the missing parameter to param_dicts.json5
    
    * Implement basic movement and proper removal for mammoths
    
    * Add mammoths to the model
    
    * Update model.py
    
    * Add mammoth counter and fix formatting
    
    * Reorder mammoth actions
    
    * Reorder grass and mammoths
    
    * Implement grazing of mammoths and add age to mammoth portrayal
    
    * Add grass boosting with 50% chance into MammothAgent.eat.
    
    * Implement get_free_cells for MammothAgent
    
    * Implement gestation logic for MammothAgent
    
    * Add the gestation attribute to the mammoth portrayal
    
    * Implement proper mammoth reproduction
    
    * Properly initialize mammoth children
    
    * Implement the MammothData dataclass
    
    * Resolve most of the review issues.
    
    * Resolve the attribute initialization issue.
    
    * Resolve the MammothAgent.step function.
    
    * Implement MammothData.__iter__ to make dict usable
    
    * Add params.py days_to_years docstring
    
    * Fix formatting in params.py
    
    * Resolve review reformatings

--- mammoth_wolf_abm/server/params.py
+++ mammoth_wolf_abm/server/params.py
@@ -1,10 +1,21 @@
 import json5
 import sys
 
-from mammoth_wolf_abm.agents import GrassAgent
+from mammoth_wolf_abm.agents import GrassAgent, MammothAgent
 from mesa.agent import Agent
 from mesa_viz_tornado.modules import CanvasGrid, ChartModule
 from mesa_viz_tornado.UserParam import *
+
+
+def days_to_years(days: int) -> str:
+    """Converts a number of days to a string of years, months, days.
+    :param int days: Number of days to convert
+    :returns str: String of years, months, days
+    """
+    y = days // 365
+    m = (days % 365) // 30
+    d = (days % 365) % 30
+    return f"{y} years, {m} months, {d} days"
 
 
 def mw_model_portrayal(agent: Agent) -> dict | None:
@@ -30,6 +41,15 @@ def mw_model_portrayal(agent: Agent) -> dict | None:
         portrayal["Layer"] = 0
         portrayal["w"] = 1
         portrayal["h"] = 1
+
+    # Mammoth portrayal
+    if isinstance(agent, MammothAgent):
+        portrayal["Shape"] = "pics/mammoth.png"
+        portrayal["Layer"] = 1
+        portrayal["Age"] = days_to_years(days=agent.age)
+        portrayal["Gestation"] = days_to_years(days=agent.gestation)
+        portrayal["Interbirth"] = days_to_years(days=agent.interbirth)
+        portrayal["EP"] = agent.energy
 
     return portrayal
 
---
+++ mammoth_wolf_abm/utils/counters.py
@@ -0,0 +1,34 @@
+from mesa.model import Model
+
+import mammoth_wolf_abm.model as abm
+from mammoth_wolf_abm.agents import GrassAgent, MammothAgent
+
+
+def count_grass_cells(model: Model) -> float:
+    """
+    Return percentage of grown grass.
+    :param MammothWolfModel model: Model whose grass filled cells are counted
+    :returns float: Percentage of cells filled with grass
+    """
+    model: abm.MammothWolfModel
+    result = 0
+    for agent in model.schedule.agents:
+        if isinstance(agent, GrassAgent):
+            agent: GrassAgent
+            if agent.grown:
+                result += 1
+    return 100 * result / float(model.grid.width * model.grid.height)
+
+
+def count_mammoths(model: Model) -> int:
+    """
+    Return the number of mammoths.
+    :param MammothWolfModel model: Model whose grass filled cells are counted
+    :returns int: Number of mammoths
+    """
+    model: abm.MammothWolfModel
+    result = 0
+    for agent in model.schedule.agents:
+        if isinstance(agent, MammothAgent):
+            result += 1
+    return result
